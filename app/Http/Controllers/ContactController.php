1c6761392d1e17cd22db6b6bbd1d2d6e7ba3ede3
pmhr18 <tmh6.pm18@gmail.com>
2021-09-25 16:18:50 +0900

    ユーザー入力内容をcontrollerでDB処理

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Contact;
 
 class ContactController extends Controller
 {
+    protected $contact;
+
+/**
+ * お問い合わせ入力情報登録
+ */
+
+    public function create(Request $requsest) :array
+    {
+        $contact = new Contact;
+        $contact->name = $request->name;
+        $contact->email = $request->email;
+        $contact->content = $request->content;
+        $contact->save();
+        return ['msg' => 'お問い合わせいただきありがとうございます'];
+    } 
     //
 }
