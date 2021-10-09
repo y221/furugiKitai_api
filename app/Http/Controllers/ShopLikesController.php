b3e15f0fe8073a11e4f6452a323fd3d516e9d226
yuta_tsutsui <ydsikohsh@gmail.com>
2021-10-10 08:04:14 +0900

    お気に入りコントローラー実装

--- app/Http/Controllers/ShopLikesController.php
+++ app/Http/Controllers/ShopLikesController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ShopLike;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ShopLikesController extends Controller
 {
-    //
+    protected $shopLike;
+    const SHOP_LIKE_VALIDATE_RULE = [
+        'userId' => 'required|integer',
+        'shopId' => 'required|integer',
+    ];
+
+    public function __construct(ShopLike $shopLike)
+    {
+        $this->shopLike = $shopLike;
+    }
+
+    public function create(Request $request) :array
+    {
+        $validator = Validator::make($request->input(), self::SHOP_LIKE_VALIDATE_RULE);
+        $shopLike = $validator->validated();
+        $this->shopLike->insertShopLike($this->myFunction->changeArrayKeySnake($shopLike));
+        return ['msg' => '登録処理が完了しました'];
+    }
 }
