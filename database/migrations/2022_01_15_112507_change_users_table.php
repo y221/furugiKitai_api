43c9dc711e1681ecb93a35a8db7d98b65fcf1879
hidetsugu-o <74892038+hidetsugu-o@users.noreply.github.com>
2022-01-15 11:39:01 +0900

    null許容して良さそうなカラムはnullableに変更

---
+++ database/migrations/2022_01_15_112507_change_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('favorite', 50)->nullable()->change();
+            $table->string('profile', 100)->nullable()->change();
+            $table->string('instagram', 100)->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
