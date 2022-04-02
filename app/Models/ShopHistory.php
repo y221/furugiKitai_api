92239b705772d0dd50166d60e8cfcd2f9f5a4943
筒井雄太 <yuta_tsutsui@tsutsuiyuutanoMacBook-Air.local>
2022-04-02 16:03:47 +0900

    履歴登録

--- app/Models/ShopHistory.php
+++ app/Models/ShopHistory.php
@@ -2,10 +2,28 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Shop;
 
 class ShopHistory extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'shop_id',
+        'name',
+        'prefecture_id',
+        'area_id',
+        'city',
+        'address',
+        'building',
+        'access',
+        'latitude',
+        'longitude',
+        'phone_number',
+        'instagram_url',
+        'holiday',
+        'business_hour',
+        'image_url',
+        'created_user_id',
+        'updated_user_id'
+    ];
 }
